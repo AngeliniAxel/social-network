85fd183f61a7b7844486652f7ef3119c3f966741
AngeliniAxel <axel.angelini@gmail.com>
2025-01-19 13:41:54 +0100

    Added Tailwind

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link';
 import React, { FC, PropsWithChildren } from 'react';
 
 const UsersLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
-      <div>ENCABEZADO MAIN</div>
+      <header className='flex justify-between mb-4 px-8 py-4 bg-gray-100'>
+        <div>LOGO</div>
+        <div className='flex'>
+          <div>
+            <Link href='/users'>Users</Link>
+          </div>
+          <div className='ml-4'>
+            <Link href='/messages'>Messages</Link>
+          </div>
+        </div>
+      </header>
       <main>{children}</main>
       <div>PIE DE PAGINA MAIN</div>
     </div>
