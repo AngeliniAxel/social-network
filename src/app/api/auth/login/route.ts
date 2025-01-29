399807e258016cea3b101b9a91eb5f0601635ffd
AngeliniAxel <axel.angelini@gmail.com>
2025-01-29 17:59:57 +0100

    Added Cookies to login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
 import authApi from '@/services/auth/auth.service';
 import { AccessDeniedError } from '@/services/common/http.errors';
-import { NextResponse, type NextRequest } from 'next/server';
+import { type NextRequest } from 'next/server';
 import { createClient } from 'redis';
 import { v4 as uuidv4 } from 'uuid';
 import * as yup from 'yup';
@@ -20,18 +20,24 @@ client.connect().then(() => {
   console.log('connected to redis');
 });
 
-const ONE_MINUTE = 60;
+const TEN_MINUTES = 60 * 10;
 
 export async function POST(request: NextRequest) {
   const { username, password } = await schema.validate(await request.json());
   try {
     const loginResponse = await authApi.login(username, password);
-
     const sessionId = uuidv4();
+    const now = new Date();
+    const expireAt = new Date(now.getTime() + TEN_MINUTES * 1000).toUTCString();
 
-    client.set(sessionId, loginResponse.accessToken, { EX: ONE_MINUTE });
+    client.set(sessionId, loginResponse.accessToken, { EX: TEN_MINUTES });
 
-    return NextResponse.json({ sessionId, username });
+    const authCookie = `SocialSessionID=${sessionId}; Expires=${expireAt} Domain=localhost; Secure: httpOnly`;
+
+    return new Response(JSON.stringify(loginResponse.user), {
+      status: 200,
+      headers: { 'Set-Cookie': authCookie },
+    });
   } catch (e) {
     if (e instanceof AccessDeniedError) {
       return new Response(
